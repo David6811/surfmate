09cf798f55952169539fc18d49b0a1cea17e7bba
weixu <xuwei19850423@gmail.com>
2025-10-14 19:03:51 +1100

    update key

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@
 
 export const supabaseConfig = {
   url: 'https://ziminskffpfzvgsbdqwn.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppbWluc2tmZnBmenZnc2JkcXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzNzM3NDEsImV4cCI6MjA3NTk0OTc0MX0.qhxrDIBZO7d7sUJGi2a4NHcHrBdXefbGTQaWJLJJpzY'
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppbWluc2tmZnBmenZnc2JkcXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3ODA4NjQsImV4cCI6MjA3NDM1Njg2NH0.E9LpY7MODpHiv79YbI8vjR1_GL4BDlXolnMkUOUqipg'
 }
